0eeb0fb8fcba79ab646d012e2ad52f2a212fff6d
Anna Erdi <erdianna607@gmail.com>
2023-12-27 16:37:00 +0100

    Format code with rustfmt

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -3,7 +3,6 @@
 /// Time:        53     89     76     98
 /// Distance:   313   1090   1214   1201
 
-
 fn main() {
     part_1();
     part_2();
@@ -14,11 +13,14 @@ fn part_1() {
     let b = calculate_number_of_ways(89, 1090);
     let c = calculate_number_of_ways(76, 1214);
     let d = calculate_number_of_ways(98, 1201);
-    println!("Result for part 1 is {}", a*b*c*d);
+    println!("Result for part 1 is {}", a * b * c * d);
 }
 
 fn part_2() {
-    println!("Result for part 2 is {}", calculate_number_of_ways(53897698, 313109012141201));
+    println!(
+        "Result for part 2 is {}",
+        calculate_number_of_ways(53897698, 313109012141201)
+    );
 }
 
 fn calculate_number_of_ways(time: i64, best_distance: i64) -> i64 {
@@ -33,8 +35,4 @@ fn calculate_number_of_ways(time: i64, best_distance: i64) -> i64 {
     }
 
     number_of_ways
-
 }
-
-
-
